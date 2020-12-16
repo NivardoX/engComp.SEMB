7e17f4eca2e7bf42f499dbed0faf80235642e44c
Nivardo <nivardo00@gmail.com>
2020-12-16 10:17:18 -0300

    DEV - separaçãõ em módulos

--- boruvkas/boruvka.h
+++ boruvkas/boruvka.h
@@ -1,9 +1,16 @@
+//
+// Created by paulo on 15/12/2020.
+//
+
+#ifndef ENGCOMP_SEMB_BORUVKA_H
+#define ENGCOMP_SEMB_BORUVKA_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
-#define MaxDim  100
-#define qnt_vertices 100
+#define MaxDim  45
+#define qnt_vertices 45
 
 // ------ STRUCTS ------
 struct Grupo
@@ -18,64 +25,48 @@ struct Aresta
 
 // ----- PROTOTIPOS ------
 
-void gera_grafo_simetrico(int);
-void printar_grafo(int[MaxDim][MaxDim],int n);
-
-void print_array(int *lista,int n);
 int encontrar_raiz_grupo(struct Grupo grupos[qnt_vertices], int i);
 void unir_grupos(struct Grupo grupos[], int x, int y);
 
-void boruvka();
+int boruvka(int vertices, int [MaxDim][MaxDim], struct Aresta[MaxDim]);
 
 // ----- GLOBAIS ------
 
 int matriz_adjacencia[MaxDim][MaxDim];
-int qnt_arestas = 0;
+int qnt_arestas = 2025;
 
 //----------------------
 
 
-
-int main()
+int boruvka(int vertices, int matriz_adjacencia_aux[MaxDim][MaxDim], struct Aresta resposta[MaxDim])
 {
-    // Gerando um grafo aleatorio(o output vai para a variavel global matriz_adjacencia)
-    gera_grafo_simetrico(qnt_vertices);
-
-    // Printando o grafo
-    printar_grafo(matriz_adjacencia, qnt_vertices);
-
-
-    printf("<Boruvka> \n");
-
-    // Rodando o algoritimo
-    boruvka();
-
-    return(0);
-}
-
-
-
-void boruvka()  
-{
+    if (vertices > MaxDim) {
+        return -1;
+    }
     // Inicializacao de variaveis auxiliares
     int menores_distancias_vertices[qnt_vertices];
     struct Aresta arestas[qnt_arestas];
     struct Grupo grupos[qnt_vertices];
 
 
-    int qnt_grupos = qnt_vertices - 1;
+
+    int resp_index = 0;
+    int iteracoes = 0;
+    int qnt_grupos = vertices - 1;
     int agm_peso = 0;
-    int qnt_arestas_aux =0;
+    int qnt_arestas_aux = 0;
 
     // Procura todas as arestas da matriz de adjacencia e salva no array
     int i,j;
 
-    for(i=0; i<qnt_vertices; i++)
+    for(i=0; i<vertices; i++)
     {
         // j <= i , pois temos um grafo simetrico
         for(j=0; j<=i; j++)
         {
-            if(matriz_adjacencia[i][j]){
+            matriz_adjacencia[i][j] = matriz_adjacencia_aux[i][j];
+            if(matriz_adjacencia[i][j])
+            {
                 struct Aresta aresta;
                 aresta.origem = i;
                 aresta.destino = j;
@@ -87,29 +78,31 @@ void boruvka()
     }
 
 
-    printf("%d arestas Foram encontras\n",qnt_arestas_aux );
-
-
-
     // Inicializa todos os grupos e seta a menor distancia deles como -1
-    for (int v = 0; v < qnt_vertices; ++v)
+    for (int v = 0; v < vertices; ++v)
     {
-        grupos[v].nivel = 0;
-        grupos[v].pai = v;
+        struct Grupo grupo;
+        grupo.nivel = 0;
+        grupo.pai = v;
+        grupos[v] = grupo;
     }
 
-
     // Enquanto houver mais de um grupo de arestas
-    while(qnt_grupos > 1){
+    while(qnt_grupos > 0){
+        if (iteracoes == vertices) {
+            return -1;
+        } else {
+            iteracoes++;
+        }
 
         // Seta todas as os vertices com menores distancias como -1
-        for (int i = 0; i < qnt_vertices; i++)
+        for (int i = 0; i < vertices; i++)
         {
             menores_distancias_vertices[i] = -1;
         }
 
 
-        for (int i=0; i< qnt_arestas; i++)
+        for (int i=0; i < qnt_arestas_aux; i++)
         {
 
             int raiz_grupo1 = encontrar_raiz_grupo(grupos, arestas[i].origem);
@@ -118,7 +111,7 @@ void boruvka()
             if (raiz_grupo1 == raiz_grupo2)
                 continue;
 
-            // Checa se o peso da aresta atual eh menor que o menor peso dos grupos
+                // Checa se o peso da aresta atual eh menor que o menor peso dos grupos
             else
             {
                 if (menores_distancias_vertices[raiz_grupo1] == -1 ||
@@ -132,7 +125,7 @@ void boruvka()
         }
 
 
-        for (int i=0; i < qnt_vertices; i++)
+        for (int i=0; i < vertices; i++)
         {
             // Checa se a menor distacia do grupo ja existe
             if (menores_distancias_vertices[i] != -1)
@@ -144,10 +137,11 @@ void boruvka()
                     continue;
                 agm_peso += arestas[menores_distancias_vertices[i]].peso;
 
-                printf("Incluindo aresta [%d][%d] na AGM, com peso: %d\n",
-                       arestas[menores_distancias_vertices[i]].origem,
-                       arestas[menores_distancias_vertices[i]].destino,
-                       arestas[menores_distancias_vertices[i]].peso);
+                struct Aresta aresta;
+                aresta.origem = arestas[menores_distancias_vertices[i]].origem;
+                aresta.destino = arestas[menores_distancias_vertices[i]].destino;
+                aresta.peso = arestas[menores_distancias_vertices[i]].peso;
+                resposta[resp_index++] = aresta;
 
                 // Une os grupos 1 e 2 e diminui a quantidade de grupos
                 unir_grupos(grupos, raiz_grupo1, raiz_grupo2);
@@ -156,42 +150,8 @@ void boruvka()
         }
 
     }
-        printf("O Peso da AGM eh: %d \n", agm_peso);
 
-}
-
-
-// Gera uma matriz de adjacencia de um grafo simetrico com valores de no max 300
-void gera_grafo_simetrico(int n)
-{
-    int i,j;
-
-    printf("\n\n Gerando Grafo Aleatorio");
-
-    // Injeta seed com o tempo atual
-    srand(time(NULL));
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = i; j < n; j++)
-        {
-            if(i == j){
-                matriz_adjacencia[i][j] = 0;
-            }
-
-            else{
-                int val = rand() % 300;
-                val = val<(int)(300/1.2)?0:val;
-                val = val == 0? 0:(rand() % 300);
-                if(val)
-                    qnt_arestas++;
-
-                matriz_adjacencia[i][j] = val;
-                matriz_adjacencia[j][i] = val;
-            }
-
-        }
-    }
-
+    return agm_peso;
 }
 
 // Funcao para realizar um Find em um grupo(arvore), ela encontra a raiz da arvore
@@ -215,7 +175,7 @@ int encontrar_raiz_grupo(struct Grupo grupos[qnt_vertices], int i)
     return grupos[i].pai;
 }
 
-// Funcao utilitaria para realizar uma Union em dois grupos(arvores), ela eh feita por nivel 
+// Funcao utilitaria para realizar uma Union em dois grupos(arvores), ela eh feita por nivel
 void unir_grupos(struct Grupo grupos[], int x, int y)
 {
 
@@ -230,7 +190,7 @@ void unir_grupos(struct Grupo grupos[], int x, int y)
     else if (grupos[raiz_grupo_x].nivel > grupos[raiz_grupo_y].nivel)
         grupos[raiz_grupo_y].pai = raiz_grupo_x;
 
-        // se estiverem no mesmo nivel escolhe um grupo aleatorio para ser o pai 
+        // se estiverem no mesmo nivel escolhe um grupo aleatorio para ser o pai
     else
     {
         grupos[raiz_grupo_y].pai = raiz_grupo_x;
@@ -238,35 +198,4 @@ void unir_grupos(struct Grupo grupos[], int x, int y)
     }
 }
 
-
-
-// Funcao utilitaria para printar a matriz de adjacencia do grafo
-void printar_grafo(int grafo[MaxDim][MaxDim],int n)
-{
-    int i,j;
-    printf("\n GRAFO: Matriz de Adjacencia\n");
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<n; j++)
-        {
-            printf("%03d ",grafo[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
-
-
-// Funcao utilitaria para printar listas
-void print_array(int *lista,int n)
-{
-    int i;
-    printf("\n Lista:\n");
-
-    for(i=0; i<n; i++)
-    {
-        printf("%d ",lista[i]);
-    }
-    printf("\n");
-
-}
+#endif //ENGCOMP_SEMB_BORUVKA_H
